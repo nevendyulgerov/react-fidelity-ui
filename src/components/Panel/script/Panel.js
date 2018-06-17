44121d393f4ffddd9ae8c7fe94ac4bc41978fd0c
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-06-17 10:12:14 +0300

    Add component Panel

--- src/components/Panel/script/Panel.js
+++ src/components/Panel/script/Panel.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { isFunc } from '../../../utils/ammo';
 
 const Panel = ({ theme = 'default', item }) => {
-  const { name, date } = item;
+  const { name, date, thumbnail, note } = item;
 
   return (
     <div data-component="panel" data-theme={theme}>
@@ -9,9 +10,35 @@ const Panel = ({ theme = 'default', item }) => {
         <div className="meta">
           <span className="name">{name}</span>
           <span className="date">
-          {`Created at `}
+            {'Created at '}
             <time>{date}</time>
-        </span>
+          </span>
+        </div>
+      )}
+      {theme === 'card' && (
+        <div className="card">
+          <figure className="avatar" title="Avatar">
+            <img
+              src={thumbnail}
+              alt="avatar"
+              onLoad={({ target }) => target.closest('.avatar').classList.add('active')}
+            />
+          </figure>
+          <div className="meta">
+            <span className="author" title="Name">
+              {name}
+            </span>
+            <span className="note">
+              {isFunc(note) ? note() : note}
+            </span>
+            <time className="date" title="Date">
+              {' at '}
+              <span className="time">
+                {item.date}
+              </span>
+              {'.'}
+            </time>
+          </div>
         </div>
       )}
     </div>
