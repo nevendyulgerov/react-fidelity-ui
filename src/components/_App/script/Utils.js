32686d0a58b685ef425fcbe01f3c4490dbc0f23b
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-06-24 10:54:02 +0300

    Add utils for component App

---
+++ src/components/_App/script/Utils.js
@@ -0,0 +1,24 @@
+import moment from 'moment/moment';
+import { randomInclusive } from '../../../utils/ammo';
+
+const defaultThumbnail = 'https://wallpaperbits.com/wp-content/uploads/2018/01/google-nexus-5-wallpaper-154171-nature-landscape-mountain-nexus-5.jpg';
+
+export const createItems = (count = 10) => {
+  const items = [];
+  for (let i = 0; i < count; i++) {
+    const isNewDate = randomInclusive(0, 10) > 6;
+    const subTrackedDays = randomInclusive(0, 365);
+    items.push({
+      id: i,
+      name: `Item ${i}`,
+      url: 'https://google.com',
+      isSelected: i === 0,
+      thumbnail: defaultThumbnail,
+      note: 'some note',
+      created_at: isNewDate
+        ? moment(new Date()).subtract(subTrackedDays, 'days')
+        : moment(i > 0 ? items[i - 1].created_at : new Date())
+    });
+  }
+  return items;
+};
