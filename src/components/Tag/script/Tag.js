f7c306067675eb86addfa70cd9f9cd5edbb225c2
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-06-24 17:27:45 +0300

    Extend component Tag

--- src/components/Tag/script/Tag.js
+++ src/components/Tag/script/Tag.js
@@ -4,20 +4,22 @@ import { isFunc } from '../../../utils/ammo';
 
 const Tag = ({ name, onRemove }) => (
   <div data-component="tag" title={`Tag '${name}'`}>
-    <span className="tag-name">{name}</span>
-    {isFunc(onRemove) && (
-      <button
-        className="trigger remove-tag"
-        title={`Remove tag '${name}'`}
-        onClick={event => {
-          event.preventDefault();
-          event.stopPropagation();
-          onRemove();
-        }}
-      >
-        <Icon name="add" />
-      </button>
-    )}
+    <div className="component-body">
+      <span className="tag-name">{name}</span>
+      {isFunc(onRemove) && (
+        <button
+          className="trigger remove-tag"
+          title={`Remove tag '${name}'`}
+          onClick={event => {
+            event.preventDefault();
+            event.stopPropagation();
+            onRemove();
+          }}
+        >
+          <Icon name="add" />
+        </button>
+      )}
+    </div>
   </div>
 );
 
