b5e1de4b124cbbbfebfbebe8854d62c56a8e9bc5
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-06-24 18:38:11 +0300

    Provide default icon option;
    Remove obsolete data;

--- src/components/Icon/script/Icon.js
+++ src/components/Icon/script/Icon.js
@@ -1,36 +1,13 @@
 import React from 'react';
 
-const icons = [
-  'add',
-  'comment',
-  'dropdown',
-  'filter',
-  'grid',
-  'more',
-  'next',
-  'notification',
-  'previous',
-  'remove',
-  'search',
-  'settings',
-  'stats',
-  'summary',
-  'thumbs-down',
-  'thumbs-up',
-  'tick'
-];
-
-const Icon = ({ name, className = '', title = '' }) => {
-
-  return (
-    <div className={`component ${className}`} data-component="icon" title={title}>
-      <svg className="icon component-icon">
-        <use xlinkHref={`./src/icons/icon-sprite.svg#icon-${name}`}>
-          {/* icon is populated here */}
-        </use>
-      </svg>
-    </div>
-  )
-};
+const Icon = ({ name = 'comment', className = '', title = '' }) => (
+  <div className={`component ${className}`} data-component="icon" title={title}>
+    <svg className="icon component-icon">
+      <use xlinkHref={`./src/icons/icon-sprite.svg#icon-${name}`}>
+        {/* icon is populated here */}
+      </use>
+    </svg>
+  </div>
+);
 
 export default Icon;
