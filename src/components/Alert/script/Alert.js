b53215500a826af482d47894e94ce4187c80991c
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-06-24 10:52:31 +0300

    Add component Alert

---
+++ src/components/Alert/script/Alert.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import Icon from '../../Icon';
+import { isHovered } from '../../../utils/ammo';
+
+const defaultDelay = 5000;
+
+class Alert extends Component {
+  state = {
+    isAlertVisible: this.props.isVisible
+  };
+
+  componentDidMount() {
+    const { isConfirm, delay = defaultDelay } = this.state;
+    if (!isConfirm) {
+      this.hideAfterDelay(delay);
+    }
+  }
+
+  componentWillReceiveProps({ isVisible, isConfirm, delay }) {
+    this.setState({ isAlertVisible: isVisible }, () => {
+      if (!isConfirm) {
+        this.hideAfterDelay(delay);
+      }
+    });
+  }
+
+  refComponent;
+
+  hideAfterDelay = delay => {
+    setTimeout(() => {
+      const isAlertHovered = isHovered(this.refComponent);
+      if (isAlertHovered) {
+        return this.hideAfterDelay(delay);
+      }
+      this.setState({ isAlertVisible: false });
+    }, delay);
+  };
+
+  render() {
+    const {
+      type = 'info',
+      title = '',
+      content = '',
+      closeText = 'Close',
+      cancelText = 'Cancel',
+      confirmText = 'OK',
+      isConfirm = false,
+      onCancel = () => {},
+      onConfirm = () => {}
+    } = this.props;
+    const { isAlertVisible } = this.state;
+
+    return isAlertVisible && (
+      <div
+        data-component="alert"
+        data-alert-type={type}
+        ref={node => {
+          this.refComponent = node;
+        }}
+      >
+        <div className="component-header">
+          <span className="title">
+            {title}
+          </span>
+          <div className="content">
+            {content}
+          </div>
+
+          <button
+            className="trigger close-alert"
+            title={closeText}
+            onClick={onCancel}
+          >
+            <Icon name="add" />
+          </button>
+        </div>
+        {isConfirm && (
+          <div className="component-body">
+            <button
+              className="trigger cancel-alert"
+              title={cancelText}
+              onClick={onCancel}
+            >
+              {cancelText}
+            </button>
+            <button
+              className="trigger confirm-alert"
+              title={confirmText}
+              onClick={onConfirm}
+            >
+              {confirmText}
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Alert;
