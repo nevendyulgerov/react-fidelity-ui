e3b2d8ec493d9a12dd30b11daa78570e89b707a1
Neven Dyulgerov <neven.diulgerov@gmail.com>
2018-07-28 18:32:00 +0300

    Update test method

--- src/components/_App/script/Utils.js
+++ src/components/_App/script/Utils.js
@@ -12,10 +12,12 @@ export const createItems = (count = 10) => {
     items.push({
       id: i,
       name: `Item ${i + 1}`,
+      title: `Item ${i + 1}`,
       url: 'https://google.com',
       isSelected,
       thumbnail: defaultThumbnail,
       note: 'some note',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       created_at: isNewDate
         ? moment(new Date()).subtract(subTrackedDays, 'days')
         : moment(i > 0 ? items[i - 1].created_at : new Date())
